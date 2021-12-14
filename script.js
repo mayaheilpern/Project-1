5c43c0763c8ec4a70bf0b2f735ba00aa42568139
Maya Heilpern <maya@maya-heilpern.myfiosgateway.com>
2021-12-14 16:48:41 -0500

    basic css

--- script.js
+++ script.js
@@ -1,33 +1,29 @@
 async function getInput(gif) {
-  try {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=RwcmcWQsdLYKrhxc8DwUjYNqy33d7jpW&q=${gif}&limit=1`;
-    const response = await axios.get(url);
-    // console.log(response);
-    const gifData = response.data.data;
-    gifData.forEach(gif => {
-      showData(gif);
-    });
-  } catch (error) {
-    showErrorMessage();
-  }
+  // try {
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=RwcmcWQsdLYKrhxc8DwUjYNqy33d7jpW&q=${gif}`;//&limit=25
+  const response = await axios.get(url);
+  console.log(response);
+  const gifData = response.data.data;
+  gifData.forEach(gif => {
+    showData(gif);
+  });
+  // } catch (error) {
+  //   showErrorMessage();
+  // }
 }
 
-function showErrorMessage() {
-  const errorImg = document.createElement("img");
-  errorImg.src = "https://media2.giphy.com/media/Ke8JKfxe83FpLrra71/giphy.gif?cid=790b7611ccb80e4d2d4b216aa10d6c1c39ff0b7c6f246fd0&rid=giphy.gif&ct=g";
-  errorImg.alt = "404 error";
-  gifList.appendChild(errorImg);
-}
+// function showErrorMessage() {
+//   const errorImg = document.createElement("img");
+//   errorImg.src = "https://media2.giphy.com/media/Ke8JKfxe83FpLrra71/giphy.gif?cid=790b7611ccb80e4d2d4b216aa10d6c1c39ff0b7c6f246fd0&rid=giphy.gif&ct=g";
+//   errorImg.alt = "404 error";
+//   gifList.appendChild(errorImg);
+// }
 
-getInput("red");
+getInput("amnbvcxzsdfghjiuytresd");
 
 const gifList = document.querySelector(".gif-list");
 
 function showData(data) {
-  // const gifA = document.createElement("a");
-  // gifA.href = `${data.images.fixed_height.url}`;
-  // gifList.appendChild(gifA);
-  // console.log(gifA);
 
   const gifImg = document.createElement("img");
   gifImg.src = `${data.images.fixed_height.url}`;
@@ -44,13 +40,6 @@ function showData(data) {
   //   callback(dataURL);
   // }
 
-  // const urlToObject = async () => {
-  //   const response = await fetch(image);
-  //   image = gifImg;
-  //   const blob = await response.blob();
-  //   const file = new File([blob], 'image.jpg', { type: blob.type });
-  //   console.log(file);
-  // }
 }
 
 // const dbtn = document.createElement("button");
